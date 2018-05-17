8d108f43f595533c8f44374b9d93b7be379c139f
Manuel Gil <mauelyeixon@gmail.com>
2018-05-17 17:04:52 -0400

    parsing y agregar departamento...

--- js/main/app.js
+++ js/main/app.js
@@ -1,21 +1,29 @@
 (function(){
 
+	var base_url = 'http://localhost:8080/tesis3/rest/';
+
+
+	$('.show-employee-form').click(function() {
+		$('.save-employees').fadeIn();
+		$('.save-department').fadeOut();
+	});
+
 	$('.save-employees').submit(function() {
 		$.ajax({
-			url: 'http://localhost:8080/tesis3/rest/insertarUsuario',
+			url: base_url + 'insertarUsuario',
 			type: 'GET',
 			dataType: 'json',
 			data: {
-				cod: $('#codigo').val(),
+				cod: parseInt($('#codigo').val()),
 				nombre: $('#nombre').val(),
 				cedula: $('#id').val(),
-				sexo: $('#sexo').val(),
+				sexo: parseInt($('#sexo').val()),
 				fecha: $('#birthDate').val(),
 				celular: $('#cel').val(),
 				telefono: $('#hab').val(),
 				email: $('#email').val(),
 				puesto: $('#puesto').val(),
-				dept: $('#departamento').val(),
+				dept: parseInt($('#departamento').val()),
 				direc: $('#address').val(),
 			},
 
@@ -23,10 +31,39 @@
 				console.log('success', response);
 				window.location.reload();
 			},
+
 			error: function (error) {
 				console.error('error', error);
 			}
 		});
+	});
+
+	$('.show-department-form').click(function() {
+		$('.save-department').fadeIn();
+		$('.save-employees').fadeOut();
+	});
+
+	$('.save-department').submit(function() {
+		$.ajax({
+			url: base_url + 'insertarDepartamento',
+			type: 'GET',
+			dataType: 'json',
+			data: {
+				description: $('#idDep').val(),
+				supervisor: $('#nombreDep').val(),
+				superemail: $('#supervisor').val(),
+			},
+
+			success: function (response) {
+				console.log('success', response);
+				window.location.reload();
+			},
+
+			error: function (error) {
+				console.error('error', error);
+			}
+		});
+		
 	});
 
 })();
