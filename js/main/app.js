4244e27e6d9ca97c95c84a225a2622e1e4532f29
Manuel Gil <mauelyeixon@gmail.com>
2018-05-17 17:51:13 -0400

    listando empleados...

--- js/main/app.js
+++ js/main/app.js
@@ -66,24 +66,51 @@
 		
 	});
 
-	$.ajax({
-		url: base_url + 'buscarDepartamento',
-		type: 'GET',
-		dataType: 'json',
+	$('.department-dd').ready(function () {
+		$.ajax({
+			url: base_url + 'buscarDepartamento',
+			type: 'GET',
+			dataType: 'json',
 
-		success: function (response) {
-			console.log('success', response);
-			$.each(response, function (i, department) {
-			    $('.department-dd').append($('<option>', { 
-			        value: department.Id,
-			        text : department.Descripcion 
-			    }));
-			});
-		},
+			success: function (response) {
+				console.log('success', response);
+				$.each(response, function (i, department) {
+				    $('.department-dd').append($('<option>', { 
+				        value: department.Id,
+				        text : department.Descripcion 
+				    }));
+				});
+			},
 
-		error: function (error) {
-			console.error('error', error);
-		}
+			error: function (error) {
+				console.error('error', error);
+			}
+		});
+	});
+
+	$('.employees-table').ready(function () {
+		$.ajax({
+			url: base_url + 'listadoEmpleados',
+			type: 'GET',
+			dataType: 'json',
+
+			success: function (response) {
+				console.log('success', response);
+				
+				$.each(response, function (i, employee) {
+				    $('.employees-table').append(`<tr>
+				        <td>` + employee.Id + `</td>
+				        <td>` + employee.Nombre + `</td>
+				        <td>` + employee.Cargo + `</td>
+				        <td>` + employee.Departamento + `</td>
+				    </tr>`);
+				});
+			},
+
+			error: function (error) {
+				console.error('error', error);
+			}
+		});
 	});
 
 })();
